7dd7839929a940ccdfe954fac37193660efb36cf
olga <olga@local>
2026-01-07 22:33:27 +0300

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,23 @@
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import { Layout } from './components/Layout'
+import { CatalogPage } from './pages/CatalogPage'
+
+function App() {
+
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={ <Layout/>} />
+        <Route path="/catalog.html" element={<CatalogPage/>} />
+        <Route path="/about.html" element={<>AboutPage </>} />
+        <Route path="/contacts.html" element={<>ContactsPage </>} />
+        <Route path="/cart.html" element={<>CartPage</>} />
+        <Route path="*" element={<div>404</div>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,84 @@
+import { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const Header = () => {
+    const navigate = useNavigate();
+
+    return (
+        <header className="container">
+            <div className="row">
+                <div className="col">
+                    <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                        <a className="navbar-brand"
+                            onClick={useCallback(() => {
+                                navigate("/", {
+                                    replace: true,
+                                })
+                            }, [navigate])}>
+                            <img src="./img/header-logo.png" alt="Bosa Noga" />
+                        </a>
+                        <div className="collapase navbar-collapse" id="navbarMain">
+                            <ul className="navbar-nav mr-auto">
+                                <li className="nav-item">
+                                    <a className="nav-link"
+                                        onClick={useCallback(() => {
+                                            navigate("/", {
+                                                replace: true,
+                                            })
+                                        }, [navigate])}>
+                                        Главная</a>
+                                </li>
+                                <li className="nav-item active">
+                                    <a className="nav-link"
+                                        onClick={useCallback(() => {
+                                            navigate("/catalog.html", {
+                                                replace: false,
+                                            })
+                                        }, [navigate])}>
+                                        Каталог</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link"
+                                        onClick={useCallback(() => {
+                                            navigate("/about.html", {
+                                                replace: false,
+                                            });
+                                        }, [navigate])}>
+                                        О магазине</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link"
+                                        onClick={useCallback(() => {
+                                            navigate("/contacts.html", {
+                                                replace: false,
+                                            });
+                                        }, [navigate])}>
+                                        Контакты</a>
+                                </li>
+                            </ul>
+                            <div>
+                                <div className="header-controls-pics">
+                                    <div data-id="search-expander" className="header-controls-pic header-controls-search"></div>
+                                    {/*<!-- Do programmatic navigation on click to /cart.html -->*/}
+                                    <div className="header-controls-pic header-controls-cart"
+                                        onClick={useCallback(() => {
+                                            navigate("/cart.html", {
+                                                replace: false,
+                                            });
+                                        }, [navigate])}
+                                    >
+                                        <div className="header-controls-cart-full">1</div>
+                                        <div className="header-controls-cart-menu"></div>
+                                    </div>
+                                </div>
+                                <form data-id="search-form" className="header-controls-search-form form-inline invisible">
+                                    <input className="form-control" placeholder="Поиск" />
+                                </form>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </header>
+    )
+}
