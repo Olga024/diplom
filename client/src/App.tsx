607b46b0fc92288db6d1d9c61181cbabfacebe00
olga <olga@local>
2026-01-10 02:18:02 +0300

    add content pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,21 +2,21 @@ import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import { Layout } from './components/Layout'
 import { CatalogPage } from './pages/CatalogPage'
+import { NotFoundPage } from './pages/NotFoundPage'
+import { AboutPage } from './pages/AboutPage'
+import { ContactsPage } from './pages/ContactsPage'
 
 function App() {
 
-
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layout />} />
-        <Route path="/catalog.html" element={<CatalogPage />} />
-        <Route path="/about.html" element={<>AboutPage </>} />
-        <Route path="/contacts.html" element={<>ContactsPage </>} />
-        <Route path="/cart.html" element={<>CartPage</>} />
-        <Route path="*" element={<div>404</div>} />
-      </Routes>
-    </>
+    <Routes>
+      <Route path="/" element={<Layout />} />
+      <Route path="/catalog.html" element={<CatalogPage />} />
+      <Route path="/about.html" element={<AboutPage />} />
+      <Route path="/contacts.html" element={<ContactsPage />} />
+      <Route path="/cart.html" element={<>CartPage</>} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   )
 }
 
---
+++ client/src/pages/NotFoundPage.tsx
@@ -0,0 +1,12 @@
+import { Footer } from "../components/Footer"
+import { Header } from "../components/Header"
+import { NotFound } from "../components/NotFound"
+
+export const NotFoundPage = () => {
+
+    return <>
+        <Header />
+        <NotFound />
+        <Footer />
+    </>
+}
